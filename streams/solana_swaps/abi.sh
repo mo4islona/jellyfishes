c60af71e150040cb21e93730d3c287aef519e458
Eugene Formanenko <mo4islona@gmail.com>
2025-02-28 15:17:03 +0400

    fix: update ABI commands for Orca and Meteora to reflect new naming conventions

--- streams/solana_swaps/abi.sh
+++ streams/solana_swaps/abi.sh
@@ -1,9 +1,9 @@
 # Orca
-npx squid-solana-typegen abi whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc#whirlpool
+npx squid-solana-typegen abi whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc#orca_whirlpool
 
 # Meteora
-npx squid-solana-typegen abi Eo7WjKq67rjJQSZxS6z3YkapzY3eMj6Xy8X5EQVn5UaB#damm
-npx squid-solana-typegen abi LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo#dlmm
+npx squid-solana-typegen abi Eo7WjKq67rjJQSZxS6z3YkapzY3eMj6Xy8X5EQVn5UaB#meteora_damm
+npx squid-solana-typegen abi LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo#meteora_dlmm
 
 # Radium
 npx squid-solana-typegen abi CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK#raydium_clmm
