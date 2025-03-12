f40a58a06f8924e98304e48be1856d7db1da06cb
Eugene Formanenko <mo4islona@gmail.com>
2025-03-12 18:59:33 +0400

    feat: create materialized views for ETH and USDC amounts with improved token handling

--- evm_dexes/uniswap_swaps/swaps.sql
+++ evm_dexes/uniswap_swaps/swaps.sql
@@ -18,22 +18,26 @@ CREATE TABLE IF NOT EXISTS uniswap_v3_swaps_raw_v2
       ORDER BY (timestamp, transaction_index, log_index);
 
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS eth_prices_mv
-(
-             timestamp DATETIME,
+CREATE MATERIALIZED VIEW IF NOT EXISTS eth_amounts_mv
+            (
+             timestamp DateTime,
              network LowCardinality(String),
-             price Float64,
-             sign Int8
-) ENGINE CollapsingMergeTree(sign) ORDER BY (timestamp, network) POPULATE
+             eth_amount Float64,
+             usdc_amount Float64
+                ) ENGINE SummingMergeTree() ORDER BY (timestamp, network) POPULATE
 AS
-SELECT timestamp,
+SELECT toStartOfMinute(timestamp),
        network,
        CASE
            WHEN token_a IN ('0x4200000000000000000000000000000000000006', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
-               THEN abs(amount_b / nullIf(amount_a, 0))
-           ELSE abs(amount_a / nullIf(amount_b, 0))
-           END as price,
-       sign
+               THEN abs(amount_a)
+           ELSE abs(amount_b)
+           END as eth_amount,
+       CASE
+           WHEN token_a IN ('0x4200000000000000000000000000000000000006', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+               THEN abs(amount_b)
+           ELSE abs(amount_a)
+           END as usdc_amount
 from uniswap_v3_swaps_raw_v2
 WHERE (
     token_a IN ('0x4200000000000000000000000000000000000006', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
@@ -43,3 +47,29 @@ WHERE (
     token_b IN ('0x4200000000000000000000000000000000000006', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
         AND token_a IN ('0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
     );
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS base_eth_prices_mv
+(
+ timestamp DATETIME,
+ network LowCardinality(String),
+ token String,
+ price Float64,
+ sign Int8
+) ENGINE CollapsingMergeTree(sign) ORDER BY (timestamp, token, network) POPULATE
+AS
+SELECT timestamp,
+       network,
+       CASE
+           WHEN token_a IN ('0x4200000000000000000000000000000000000006', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+               THEN token_b
+           ELSE token_a
+           END as token,
+       CASE
+           WHEN token_a IN ('0x4200000000000000000000000000000000000006', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+               THEN abs(amount_b / amount_a)
+           ELSE abs(amount_a / amount_b)
+           END as price,
+       sign
+from uniswap_v3_swaps_raw_v2
+WHERE token_a IN ('0x4200000000000000000000000000000000000006', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+   OR token_b IN ('0x4200000000000000000000000000000000000006', '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2');
