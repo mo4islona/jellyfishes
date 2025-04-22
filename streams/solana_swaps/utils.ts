039a3c74c975d19d7e96cb7b3c5b01bec6138371
belopash <abelopashentsev@gmail.com>
2025-04-22 16:47:46 +0300

    feat: rework liquidity pipe

--- streams/solana_swaps/utils.ts
+++ streams/solana_swaps/utils.ts
@@ -2,9 +2,9 @@ import assert from 'assert';
 import { getInstructionData } from '@subsquid/solana-stream';
 import { toHex } from '@subsquid/util-internal-hex';
 import * as tokenProgram from './abi/tokenProgram';
+import { Instruction, Block } from '@subsquid/solana-objects';
 
-export type Instruction = any;
-export type Block = any;
+export { Instruction, Block };
 
 export function getInstructionBalances(ins: Instruction, block: Block) {
   return block.tokenBalances?.filter((t) => t.transactionIndex === ins.transactionIndex) || [];
@@ -26,10 +26,15 @@ export function getInnerTransfersByLevel(
     if (inner.transactionIndex !== parent.transactionIndex) return false;
     if (inner.instructionAddress.length !== parent.instructionAddress.length + level) return false;
     if (inner.programId !== tokenProgram.programId) return false;
-    if (getInstructionD1(inner) !== tokenProgram.instructions.transfer.d1) return false;
 
-    // All child instructions should have the same prefix as the parent
-    return parent.instructionAddress.every((v, i) => v === inner.instructionAddress[i]);
+    switch (inner.d1) {
+      case tokenProgram.instructions.transfer.d1:
+        return true;
+      case tokenProgram.instructions.transferChecked.d1:
+        return true;
+      default:
+        return false;
+    }
   });
 }
 
@@ -42,4 +47,14 @@ export function getNextInstruction(instruction: Instruction, instructions: Instr
 
 export function getInstructionD1(instruction: Instruction) {
   return toHex(getInstructionData(instruction)).slice(0, 4);
+}
+
+export function addErrorContext<T extends Error>(err: T, ctx: any): T {
+  const e = err as any;
+  for (const key in ctx) {
+    if (e[key] == null) {
+      e[key] = ctx[key];
+    }
+  }
+  return err;
 }
